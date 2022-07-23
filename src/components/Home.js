dffeb6ecd35d2e37d7b42fb62c3c510824a9a282
BEKKEM <f20170028@hyderabad.bits-pilani.ac.in>
2022-07-23 18:48:50 +0530

    clean components

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import './Home.css'
 import noPoster from '../assets/no-poster.jpg'
 import noPoster2 from '../assets/no-poster2.jpg'
+import Singlecard from './Singlecard';
 
 let cardArray = Array(15).fill(noPoster);
 cardArray[2] = noPoster2
@@ -12,24 +13,7 @@ export default function Home() {
         <div className='home'>
             {cardArray && cardArray.map((card, index) => (
                 <div key={index} className="single-card">
-                    {index !== 2 && <img src={card} alt = 'card' className='card-image' style={{width:'250px',height:'350px'}} />}
-                    {index === 2 && <img src={card} alt = 'card' className='card-image' />}
-                    <div className='card-overlay'>
-                        <button className='save-btn'>Save</button>
-                        <div className='bottom-icons'>
-                            <div className="redirect">
-                                <i className="bi bi-arrow-up-right"></i>
-                                <span> google</span>
-                            </div>
-                            <div style={{backgroundColor: 'lightgray', padding:'10px 12px',borderRadius:'50%',cursor:'pointer'}} className="download">
-                                <i className="bi bi-download"></i>
-                            </div>
-                            <div style={{backgroundColor: 'lightgray', padding:'10px 12px',borderRadius:'50%',marginLeft:'5px',cursor:'pointer', marginRight:'10px'}} className="options">
-                                <i className="bi bi-three-dots"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <div style={{padding:'5px', fontWeight:'600'}}>Title</div>
+                    <Singlecard card={card} index = {index} />
                 </div>
             ))}
         </div>
---
+++ src/components/Singlecard.js
@@ -0,0 +1,26 @@
+import './Singlecard.css'
+
+export default function Singlecard({ card, index }) {
+  return (
+    <div>
+        {index !== 2 && <img src={card} alt = 'card' className='card-image' style={{width:'250px',height:'350px'}} />}
+        {index === 2 && <img src={card} alt = 'card' className='card-image' />}
+        <div className='card-overlay'>
+            <button className='save-btn'>Save</button>
+                <div className='bottom-icons'>
+                    <div className="redirect">
+                        <i className="bi bi-arrow-up-right"></i>
+                        <span> google</span>
+                    </div>
+                    <div style={{backgroundColor: 'lightgray', padding:'10px 12px',borderRadius:'50%',cursor:'pointer'}} className="download">
+                        <i className="bi bi-download"></i>
+                    </div>
+                    <div style={{backgroundColor: 'lightgray', padding:'10px 12px',borderRadius:'50%',marginLeft:'5px',cursor:'pointer', marginRight:'10px'}} className="options">
+                        <i className="bi bi-three-dots"></i>
+                    </div>
+                </div>
+        </div>
+        <div style={{padding:'5px', fontWeight:'600'}}>Title</div>
+    </div>
+  );
+}
