f32c5e2e17629348bbe838c9e2d5a584022802ed
BEKKEM <f20170028@hyderabad.bits-pilani.ac.in>
2022-07-23 23:47:41 +0530

    minor changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,20 +4,18 @@ import noPoster2 from '../assets/no-poster2.jpg'
 import Singlecard from './Singlecard';
 
 let cardArray = Array(15).fill(noPoster);
-cardArray[2] = noPoster2
+cardArray[4] = noPoster2
 
 
 export default function Home() {
     return (
-        <>
-        <div className='home'>
-            {cardArray && cardArray.map((card, index) => (
-                <div key={index} className="single-card">
-                    <Singlecard card={card} index = {index} />
-                </div>
-            ))}
+        <div className='container'>
+            <div className='home'>
+                {cardArray && cardArray.map((card, index) => (
+                    <Singlecard card={card} index = {index} key = {index} />
+                ))}
+            </div>
+            <div className='question-mark'><i className="bi bi-question-lg"></i></div>
         </div>
-        <div className='question-mark'><i className="bi bi-question-lg"></i></div>
-        </>
     );
 }
--- src/components/Singlecard.js
+++ src/components/Singlecard.js
@@ -2,11 +2,12 @@ import './Singlecard.css'
 
 export default function Singlecard({ card, index }) {
   return (
-    <div>
-        {index !== 2 && <img src={card} alt = 'card' className='card-image' style={{width:'250px',height:'350px'}} />}
-        {index === 2 && <img src={card} alt = 'card' className='card-image' />}
-        <div className='card-overlay'>
-            <button className='save-btn'>Save</button>
+    <div className='single-card'>
+        <div className='single-image'>
+            {index !== 4 && <img src={card} alt = 'card' className='card-image' style={{width:'250px',height:'350px'}} />}
+            {index === 4 && <img src={card} alt = 'card' className='card-image' />}
+            <div className='card-overlay'>
+                <button className='save-btn'>Save</button>
                 <div className='bottom-icons'>
                     <div className="redirect">
                         <i className="bi bi-arrow-up-right"></i>
@@ -19,8 +20,9 @@ export default function Singlecard({ card, index }) {
                         <i className="bi bi-three-dots"></i>
                     </div>
                 </div>
+            </div>
         </div>
-        <div style={{padding:'5px', fontWeight:'600'}}>Title</div>
+        <div style={{padding:'5px', fontWeight:'600', width: 'fit-content'}} className='title'>Title</div>
     </div>
   );
 }
